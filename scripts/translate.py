12d777beace7ea779a49cff2db8f9bf0f89ce2d9
Mark Small <mark.small@astrosat.space>
2021-10-07 11:57:44 +0100

    feat(script): Translate CSV to GeoJSON
    This is the first round of work, it tranlates a CSV file to a GeoJSON
    file. The files to work with are hard-coded, future work will look
    to make that more configurable.

--- scripts/translate.py
+++ scripts/translate.py
@@ -2,8 +2,23 @@ import os
 import geopandas as gpd
 
 def translate(input_filename, output_filename):
-    print(f"Script called with: {input_filename} and {output_filename}", flush=True)
+    # Read the data file
+    stadiums = gpd.read_file(input_filename)
+
+    # Create a new data frame with an extra column `geometry`, using the
+    # `Latitude` and `Longitude` column as values.
+    gdf = gpd.GeoDataFrame(stadiums, geometry=gpd.points_from_xy(stadiums.Latitude, stadiums.Longitude))
+    gdf.crs = 'epsg:4326'
+
+    # Remove Latitude/Longitude columns as they are have now been combined
+    # into the geometry column.
+    gdf = gdf.drop(['Latitude', 'Longitude'], axis=1)
+
+    # Output the data frame to GeoJsON.
+    gdf.to_file(output_filename, driver='GeoJSON')
 
 
+INPUT_FILENAME = os.environ.get('INPUT', 1)
+OUTPUT_FILENAME = os.environ.get('OUTPUT', 1)
 if __name__ == '__main__':
     translate('/opt/resources/stadiums.csv', '/opt/resources/stadiums.geojson')
