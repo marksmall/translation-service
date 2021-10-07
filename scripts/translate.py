c37612b71a1bf2360535e8e8bc435fcda04e44ec
Mark Small <mark.small@astrosat.space>
2021-10-07 21:09:24 +0100

    feat(script): Add validation of columns
    Added validation to assert the CSV file has at least columns `Longitude`
    and `Latitude`. I just check for column headers, not whether the values
    are sensible lon/lat values.

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,13 +1,19 @@
 import os
 import geopandas as gpd
 
+COLUMNS = ['Longitude', 'Latitude', ]
+
 def translate(input_filename, output_filename):
     # Read the data file
-    stadiums = gpd.read_file(input_filename)
+    df = gpd.read_file(input_filename)
+
+    # Verify `Longitude` and `Latitude` columns exist.
+    for column in COLUMNS:
+        assert (column in df.columns), f"{column} must be a column in the CSV file."
 
     # Create a new data frame with an extra column `geometry`, using the
     # `Latitude` and `Longitude` column as values.
-    gdf = gpd.GeoDataFrame(stadiums, geometry=gpd.points_from_xy(stadiums.Latitude, stadiums.Longitude))
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Latitude, df.Longitude))
     gdf.crs = 'epsg:4326'
 
     # Remove Latitude/Longitude columns as they are have now been combined
@@ -21,4 +27,7 @@ def translate(input_filename, output_filename):
 INPUT_FILENAME = os.environ.get('INPUT', 1)
 OUTPUT_FILENAME = os.environ.get('OUTPUT', 1)
 if __name__ == '__main__':
+    # import pdb
+    # pdb.set_trace()
     translate('/opt/resources/stadiums.csv', '/opt/resources/stadiums.geojson')
+    # translate(INPUT_FILENAME, OUTPUT_FILENAME)
